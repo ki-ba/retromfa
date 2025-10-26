38f26785cbea562220e559489df8e7baee34d498
Corbiobio <113342618+Corbiobio@users.noreply.github.com>
2025-10-26 15:15:07 +0100

    add protection to mlx

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "retromfa.h"
 #include "mlx.h"
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <fcntl.h>
@@ -7,7 +8,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
-
+#include <X11/X.h>
 
 static void	pixel_put(t_image *img, int x, int y, unsigned int color)
 {
@@ -17,13 +18,16 @@ static void	pixel_put(t_image *img, int x, int y, unsigned int color)
 	*(unsigned int *)dst = color;
 }
 
-void	build_img(void *mlx, t_image *img, int height, int width)
+void	*build_img(void *mlx, t_image *img, int height, int width)
 {
 	img->height = height;
 	img->width = width;
 	img->img = mlx_new_image(mlx, img->width, img->height);
+	if (img->img == NULL)
+		return (NULL);
 	img->addr = mlx_get_data_addr(img->img, &img->bpp, &img->len_line, &img->endian);
 	img->bpp /= 8;
+	return (img->img);
 }
 
 unsigned char reverse(unsigned char b) {
@@ -77,14 +81,42 @@ void display_image_16(t_image img, const unsigned char *target)
 	}
 }
 
+int free_mlx(void *mlx, void *wind)
+{
+	if (wind != NULL && mlx != NULL)
+		mlx_destroy_window(mlx, wind);
+	if (mlx != NULL)
+	{
+		mlx_destroy_display(mlx);
+		free(mlx);
+	}
+	return (1);
+}
+
+int	clean_exit(void *args[])
+{
+	void *mlx = args[0];
+	void *wind = args[1];
+	mlx_loop_end(mlx);
+	free_mlx(mlx, wind);
+	return (0);
+}
+
 int	main(void)
 {
 	void* mlx = mlx_init();
+	if (mlx == NULL)
+		return (EXIT_FAILURE);
 	void* wind = mlx_new_window(mlx, WIN_WIDTH, WIND_HEIGHT, "test");
-	(void)wind;
+	if (wind == NULL)
+		return (free_mlx(mlx, NULL));
+	mlx_hook(wind, DestroyNotify, ResizeRedirectMask, &clean_exit, (void*[]){mlx, wind});
 	int fd = open("file/white2.mfa", O_RDWR);
-	char str[1147483];
-	const int bytes_read = read(fd, str, 1147483);
+	if (fd == -1)
+		return (free_mlx(mlx, wind));
+	char str[1500000];
+	const int bytes_read = read(fd, str, 1500000);
+	close(fd);
 	const uint32_t target_flag = 0x06100000;
 
 
@@ -106,7 +138,11 @@ int	main(void)
 		{
 			printf("goal !! img:%d\n", img_index);
 			t_image img;
-			build_img(mlx, &img, tmp_height, tmp_width);
+			if (build_img(mlx, &img, tmp_height, tmp_width) == NULL)
+			{
+				free_mlx(mlx, wind);
+				exit(EXIT_FAILURE);
+			}
 
 			static int offset = 13;
 			const unsigned char *target_item = (unsigned char *)str + i + offset;
@@ -130,6 +166,7 @@ int	main(void)
 				mlx_put_image_to_window(mlx, wind, img.img, x_display_offset, y_display_offset);
 				x_display_offset = x_display_offset + img.width + 30;
 			}
+			mlx_destroy_image(mlx, img.img);
 			// i += j;
 		}
 	}
